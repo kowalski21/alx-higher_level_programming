2106ef33b5ad343e5d78b67108dc526e0622a071
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-08-14 21:40:05 +0000

    final lap 3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,6 @@
 
 def no_c(my_string):
     """Remove all characters c and C from a string."""
-    new_string = [item for item in my_string if item != "C" and item != "c"]
-    return new_string
+    new_string = "".join(
+        [item for item in my_string if item != "C" and item != "c"])
+    return (new_string)
