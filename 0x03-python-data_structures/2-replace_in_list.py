d7a02bba7402b6322a45d7e3e7cd653a3426a5b9
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-08-14 19:57:16 +0000

    fixed  2-replace -1

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
+    my_list[idx] = element
+    return (my_list)
