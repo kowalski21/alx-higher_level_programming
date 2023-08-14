2bf6caee1d2fd51d49bd5221c6448423f39fac43
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-08-14 19:17:03 +0000

    0x03 fixed 8

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 # 0-print_list_integer.py
-# Douglas Amloo-Sargon <douglasbiomed3@gmail.com>
+# Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
 
 
 def print_list_integer(my_list=[]):
-    """Print all integers of a list."""
+    """Print all integers of a list.
+
+    Args:
+        my_list: list of integers
+    """
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
