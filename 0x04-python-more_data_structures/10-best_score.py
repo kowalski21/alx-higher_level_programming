742bca33d35e7ed61a4a112dd4cef976dd97fbad
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-08-16 13:10:01 +0000

    fixed it

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@
 def best_score(a_dictionary):
     """Returns a key with the biggest  value."""
     if len(a_dictionary) == 0 or not isinstance(a_dictionary):
-        return None
+        return 0
 
     max_key = list(a_dictionary.keys())[0]
     max_num = a_dictionary[max_key]
