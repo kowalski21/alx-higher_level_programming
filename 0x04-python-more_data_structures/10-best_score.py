26b975aab162ae205d16d0ac92870f528499260b
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-08-16 12:40:09 +0000

    0x04 More data structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 10-best_score.py
+
+
+def best_score(a_dictionary: dict):
+    """Returns a key with the biggest  value."""
+    if len(a_dictionary) == 0 or not isinstance(a_dictionary):
+        return None
+
+    max_key = list(a_dictionary.keys())[0]
+    max_num = a_dictionary[max_key]
+    for key, value in a_dictionary.items():
+        if value > max_num:
+            max_num = value
+            max_key = key
+    return max_key
