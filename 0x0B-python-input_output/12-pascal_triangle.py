aa9333bcec27d26183b48ededc6b5d45bc9edb61
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-09-12 22:19:07 -0700

    input and output in python

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 12-pascal_triangle.py
+
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    trang = [[1]]
+    while len(trang) != n:
+        tri = trang[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        trang.append(tmp)
+    return trang
